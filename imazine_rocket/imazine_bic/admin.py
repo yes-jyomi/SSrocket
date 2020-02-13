f8ca0c1a4e4dc1f4a9550fd19652f52a82cda06e
Daria0412 <s2018w29@icloud.com>
2020-02-13 15:47:56 +0900

    modify admin.py

--- imazine_rocket/imazine_bic/admin.py
+++ imazine_rocket/imazine_bic/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import History
+from .models import User
+from .models import Course
 # Register your models here.
+
+admin.site.register(History)
+admin.site.register(User)
+admin.site.register(Course)
--- imazine_rocket/imazine_bic/models.py
+++ imazine_rocket/imazine_bic/models.py
@@ -3,18 +3,56 @@ from django.db import models
 from django.utils import timezone
 
 
-class History(models.Model):
-    # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # title = models.CharField(max_length=200)
-    # text = models.TextField()
-    # created_date = models.DateTimeField(
-    #         default=timezone.now)
-    # published_date = models.DateTimeField(
-    #         blank=True, null=True)
+class User(models.Model):
+    id = models.CharField(db_column='id',primary_key=True,max_length=200)
+    name = models.CharField(db_column='name',max_length=200)
+    pwd = models.CharField(db_column='pwd',max_length=200)
+    info = models.IntegerField(db_column='info')
+
+    class Meta:
+        managed = False
+        db_table = 'user'
 
     def publish(self):
-        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.user_id
+
+
+class History(models.Model):
+    history_num = models.AutoField(db_column='history_num',primary_key=True)
+    history_place = models.CharField(db_column='history_place',max_length=200)
+    user_id = models.CharField(db_column='user_id',max_length=200)
+    r_btime = models.DateTimeField(db_column='r_btime')
+    btime = models.DateTimeField(db_column='btime')
+    r_rtime = models.DateTimeField(db_column='r_rtime')
+    rtime = models.DateTimeField(db_column='rtime')
+
+    class Meta:
+        managed = False
+        db_table = 'history'
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.user_id
+
+class Course(models.Model):
+    c_num = models.AutoField(db_column='c_num',primary_key=True)
+    c_name = models.CharField(db_column='c_name',max_length=200)
+    c_info = models.TextField(db_column='c_info',max_length=1000)
+    c_loc = models.CharField(db_column='c_loc',max_length=200)
+    c_theme = models.CharField(db_column='c_theme',max_length=200)
+    c_time = models.IntegerField(db_column='c_time')
+
+    class Meta:
+        managed = False
+        db_table = 'course'
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.user_id
