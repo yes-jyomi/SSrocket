902e6df47347571ecd56c5a05f6982daaeb4dd5b
South_under_been <s2018s27@e-mirim.hs.kr>
2020-02-14 21:50:38 +0900

    TEST

--- imazine_rocket/imazine_bic/templates/imazine_bic/static/js/main.js
+++ imazine_rocket/imazine_bic/templates/imazine_bic/static/js/main.js
@@ -1,24 +1,24 @@
 function imgch01(){
     var img=document.getElementById("main-bt01");
             img.src = "../img/main-btgr01.png";
-            location.href="../reservation_1.html";
+            location.href="reservation_1.html";
         }
 
 function imgch02(){
      var img=document.getElementById("main-bt02");
               img.src = "../img/main-btgr02.png";
-              location.href="../reservation_2.html";
+              location.href="reservation_2.html";
         }
 
 function imgch03(){
     var img=document.getElementById("main-bt03");
                img.src = "../img/main-btgr03.png";
-               location.href="../reservation_3.html";
+               location.href="reservation_3.html";
         }
 function imgch04(){
      var img=document.getElementById("main-bt04");
                 img.src = "../img/main-btgr04.png";
-                location.href="../reservation_end.html";
+                location.href="reservation_end.html";
         }
        
        
