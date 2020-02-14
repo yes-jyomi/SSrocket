e55292de5b90c99bb93fe3fb088b50fbe0297815
Daria0412 <s2018w29@icloud.com>
2020-02-14 13:42:55 +0900

    modified&add models

--- imazine_rocket/imazine_bic/models.py
+++ imazine_rocket/imazine_bic/models.py
@@ -17,17 +17,18 @@ class User(models.Model):
         self.save()
 
     def __str__(self):
-        return self.user_id
+        return self.id
 
 
 class History(models.Model):
     history_num = models.AutoField(db_column='history_num',primary_key=True)
     history_place = models.CharField(db_column='history_place',max_length=200)
-    user_id = models.CharField(db_column='user_id',max_length=200)
+    member_id = models.CharField(db_column='member_id',max_length=200)
     r_btime = models.DateTimeField(db_column='r_btime')
     btime = models.DateTimeField(db_column='btime')
     r_rtime = models.DateTimeField(db_column='r_rtime')
     rtime = models.DateTimeField(db_column='rtime')
+    reserved_At = models.DateTimeField(db_column='reserved_At')
 
     class Meta:
         managed = False
@@ -37,7 +38,7 @@ class History(models.Model):
         self.save()
 
     def __str__(self):
-        return self.user_id
+        return self.member_id
 
 class Course(models.Model):
     c_num = models.AutoField(db_column='c_num',primary_key=True)
@@ -55,4 +56,26 @@ class Course(models.Model):
         self.save()
 
     def __str__(self):
-        return self.user_id
+        return self.c_name
+
+
+class Company(models.Model):
+    company_num = models.AutoField(db_column='company_num',primary_key=True)
+    member_name = models.CharField(db_column='member_name',max_length=200)
+    company_addr = models.CharField(db_column='company_addr',max_length=200)
+    company_phone = models.CharField(db_column='company_phone',max_length=200)
+    bicycle_num = models.IntegerField(db_column='bicycle_num')
+    member_id = models.CharField(db_column='member_id',max_length=200)
+    s_business = models.DateTimeField(db_column='s_business')
+    e_business = models.DateTimeField(db_column='e_business')
+
+    class Meta:
+        managed = False
+        db_table = 'company'
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.member_name
+
