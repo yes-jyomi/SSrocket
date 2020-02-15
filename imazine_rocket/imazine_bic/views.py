0d098760aba0bc8f56943a32db98d75996e7a5e1
Sagittta <js1verj@gmail.com>
2020-02-15 22:26:28 +0900

    signup

--- imazine_rocket/imazine_bic/views.py
+++ imazine_rocket/imazine_bic/views.py
@@ -23,3 +23,30 @@ def reservation_3(request):
 
 def reservation_4(request):
     return render(request, 'imazine_bic/reservation_4.html')
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST["pwd"] == request.POST["rePwd"]:
+            user = User.objects.create_user(
+                username = request.POST["username"], password = request.POST["pwd"]) 
+            auth.login(request, user)
+            return redirect('home')
+        return render(request, 'imazine_bic/signup.html')
+    return render(request, 'imazine_bic/signup.html')
+
+# def login(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = auth.authenticate(request, username=username, password=password)
+#         if user is not None:
+#             auth.login(request, user)
+#             return redirect('home')
+#         else:
+#             return render(request, 'login.html', {'error': 'username or password is incorrect'})
+#     else:
+#         return render(request, 'login.html')
+
+# def logout(request):
+#     auth.logout(request)
+#     return redirect('home')
