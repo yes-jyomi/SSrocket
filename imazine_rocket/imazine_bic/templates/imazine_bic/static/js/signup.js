225e4132d7fbf44e79675741484bbfe79282229f
South_under_been <s2018s27@e-mirim.hs.kr>
2020-02-15 17:48:28 +0900

    둣ㄷㄱ

--- imazine_rocket/imazine_bic/templates/imazine_bic/static/js/signup.js
+++ imazine_rocket/imazine_bic/templates/imazine_bic/static/js/signup.js
@@ -3,11 +3,36 @@ function clearText(thefield){
                 thefield.value = ""
             }
 
-function check_pw(){
-    if($('#pw').val()==$('#repw').val()){
-
-    }
-    else{
+var check = function(el) {
+    var password = document.getElementById('pw');
+        var passwordCheck = el;
         
+        //F12 누르고 개발자 도구 Console에서 확인.
+        console.log('password=' + password.value + ', passwordCheck=' + passwordCheck.value);
+        console.log('isValid=' + password.value === passwordCheck.value);
+        
+        if (password.value !== passwordCheck.value) {
+
+            passwordCheck.value = '';
+            document.get
+            passwordCheck.placeholder = '비밀번호를 다시 확인해주세요.';
+                
+        alert('비밀번호가 일치하지 않습니다!');
+        return;
+         }
     }
-}
+
+          
+
+var echeck = function(ech) {
+    var email = document.getElementById('email');
+    email=ech;
+    var regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+              
+              if (email.value !== null && email.match(regExp) != null) {
+                  alert('회원가입 성공!');         
+              }
+              //alert('이메일 주소를 확인해주세요.');
+              email.placeholder = '비밀번호를 다시 확인해주세요.';
+              ech.placeholder='이메일을 다시 확인해주세요.';
+            }
