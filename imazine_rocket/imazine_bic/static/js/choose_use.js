226f320b86756d6ca5ce1c6515ec087d95e6de7f
South_under_been <s2018s27@e-mirim.hs.kr>
2020-02-17 02:21:49 +0900

    modify img path

---
+++ imazine_rocket/imazine_bic/static/js/choose_use.js
@@ -0,0 +1,15 @@
+function ch01fun(){
+    var img=document.getElementById("traveler");
+             img.src = "../../static/img/traveler-glow.png";
+       }
+
+function ch02fun(){
+    var img=document.getElementById("company");
+             img.src = "../../static/img/company-glow.png";
+}
+
+function ch03fun(){
+    var img=document.getElementById("nextbt");
+    img.src = "../../static/img/nextbt-glow.png";
+    location.href="signup.html";
+}
