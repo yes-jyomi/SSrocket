51e77a5a9b7a42bc77bc35201244aa024a5bc128
Daria0412 <s2018w29@icloud.com>
2020-02-16 23:12:28 +0900

    add url signup

--- imazine_rocket/imazine_bic/urls.py
+++ imazine_rocket/imazine_bic/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('main', views.index, name='index'),
     path('index_user', views.index_user, name='index_user'),
     path('index_company',views.index_company, name='index_company'),
     path('reservation_1',views.reservation_1, name='reservation_1'),
