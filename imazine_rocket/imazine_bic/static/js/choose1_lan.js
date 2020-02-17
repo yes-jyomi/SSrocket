2b32d3b5e8f7b08077604c671412edae3c4f96ef
South_under_been <s2018s27@e-mirim.hs.kr>
2020-02-17 22:04:15 +0900

    modify img path

---
+++ imazine_rocket/imazine_bic/static/js/choose1_lan.js
@@ -0,0 +1,14 @@
+function ch01fun(){
+    var img=document.getElementById("kr-bt1");
+    img.src= "../../static/img/kr-bt2.png";
+}
+
+function ch02fun(){
+var img=document.getElementById("jp-bt1");
+img.src= "../../static/img/jp-bt2.png";
+}
+
+function ch03fun(){
+    var img=document.getElementById("eg-bt1");
+    img.src= "../../static/img/eg-bt2.png";
+}
--- imazine_rocket/imazine_bic/static/js/choose_use.js
+++ imazine_rocket/imazine_bic/static/js/choose_use.js
@@ -1,10 +1,10 @@
 function ch01fun(){
     var img=document.getElementById("traveler");
-    img.src= "../img/tr-bt2.png";
-
+    img.src= "../../static/img/tr-bt2.png";
 }
+
 
 function ch02fun(){
     var img=document.getElementById("company");
-    img.src= "../img/comp-bt2.png";
+    img.src= "../../static/img/comp-bt2.png";
 }
