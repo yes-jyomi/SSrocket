fb4612b2b62d533ffac432224e2485069a9738db
Daria0412 <s2018w29@icloud.com>
2020-02-17 10:51:07 +0900

    before commit

--- imazine_rocket/imazine_bic/admin.py
+++ imazine_rocket/imazine_bic/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import History
-from .models import User
-from .models import Course
-from .models import Company
+from .models import *
 # Register your models here.
 
 admin.site.register(History)
 admin.site.register(User)
 admin.site.register(Course)
 admin.site.register(Company)
+admin.site.register(Notice)
