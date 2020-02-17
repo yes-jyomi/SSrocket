3e318bcd8809955cac85c9e41599b215617cc306
South_under_been <s2018s27@e-mirim.hs.kr>
2020-02-17 16:33:13 +0900

    modify img path

---
+++ imazine_rocket/imazine_bic/static/js/choose1_use.js
@@ -0,0 +1,10 @@
+function ch01fun(){
+    var img=document.getElementById("traveler");
+    img.src= "../img/tr-bt2.png";
+
+}
+
+function ch02fun(){
+    var img=document.getElementById("company");
+    img.src= "../img/comp-bt2.png";
+}
